038861e33de9709022a912157d908c3f896a2d46
Semyon Vyskubov <sv@rv7.ru>
2015-01-08 01:33:29 +0300

    Fixes #18, Livereload

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ var gulp           = require('gulp'),
 	notify         = require('gulp-notify'),
 	jade           = require('gulp-jade'),
 	rename         = require('gulp-rename'),
-	mainBowerFiles = require('main-bower-files');
+	mainBowerFiles = require('main-bower-files'),
+	connect        = require('gulp-connect');
 
 gulp.task('collect-components', function () {
 	gulp.src(mainBowerFiles(), {base: './bower_components'})
@@ -21,7 +22,8 @@ gulp.task('styles', function () {
 		.pipe(sourceMaps.init())
 		.pipe(sass()).on('error', notify.onError("Error: <%= error.message %>"))
 		.pipe(sourceMaps.write('./'))
-		.pipe(gulp.dest('./dist'));
+		.pipe(gulp.dest('./dist'))
+		.pipe(connect.reload());
 
 	gulp.src('./styles/styles.scss')
 		.pipe(sass()).on('error', notify.onError("Error: <%= error.message %>"))
@@ -29,13 +31,15 @@ gulp.task('styles', function () {
 			keepSpecialComments: 0
 		}))
 		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('./dist'));
+		.pipe(gulp.dest('./dist'))
+		.pipe(connect.reload());
 
 	gulp.src('./styles/styles-bower.scss')
 		.pipe(sourceMaps.init())
 		.pipe(sass()).on('error', notify.onError("Error: <%= error.message %>"))
 		.pipe(sourceMaps.write('./'))
-		.pipe(gulp.dest('./dist'));
+		.pipe(gulp.dest('./dist'))
+		.pipe(connect.reload());
 
 	gulp.src('./styles/styles-bower.scss')
 		.pipe(sass()).on('error', notify.onError("Error: <%= error.message %>"))
@@ -43,7 +47,8 @@ gulp.task('styles', function () {
 			keepSpecialComments: 0
 		}))
 		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('./dist'));
+		.pipe(gulp.dest('./dist'))
+		.pipe(connect.reload());
 });
 
 gulp.task('templates', function () {
@@ -52,7 +57,8 @@ gulp.task('templates', function () {
 			module: 'su-templates',
 			standalone: true
 		}))
-		.pipe(gulp.dest('./scripts'));
+		.pipe(gulp.dest('./scripts'))
+		.pipe(connect.reload());
 });
 
 gulp.task('scripts', function () {
@@ -61,39 +67,51 @@ gulp.task('scripts', function () {
 		.pipe(sourceMaps.init())
 		.pipe(concat('app.js'))
 		.pipe(sourceMaps.write('./'))
-		.pipe(gulp.dest('./dist'));
+		.pipe(gulp.dest('./dist'))
+		.pipe(connect.reload());
 
 	gulp.src(['./scripts/**/*.js'])
 		.pipe(ngAnnotate()).on('error', notify.onError("Error: <%= error.message %>"))
 		.pipe(concat('app.min.js'))
 		.pipe(uglify()).on('error', notify.onError("Error: <%= error.message %>"))
-		.pipe(gulp.dest('./dist'));
+		.pipe(gulp.dest('./dist'))
+		.pipe(connect.reload());
 });
 
-gulp.task('demo-views', function() {
+gulp.task('demo-views', function () {
 	gulp.src('./demo/src/views/*.jade')
 		.pipe(jade({
 			locals: require('./demo/src/config')
 		})).on('error', notify.onError("Error: <%= error.message %>"))
-		.pipe(gulp.dest('./demo/'));
+		.pipe(gulp.dest('./demo/'))
+		.pipe(connect.reload());
 });
 
-gulp.task('demo-styles', function() {
+gulp.task('demo-styles', function () {
 	gulp.src('./demo/src/styles/style.scss')
 		.pipe(sass()).on('error', notify.onError("Error: <%= error.message %>"))
 		.pipe(minifyCSS({
 			keepSpecialComments: 0
 		}))
 		.pipe(rename('styles.min.css'))
-		.pipe(gulp.dest('./demo/res'));
+		.pipe(gulp.dest('./demo/res'))
+		.pipe(connect.reload());
+});
+
+gulp.task('connect', function () {
+	connect.server({
+		root: './',
+		port: 3030,
+		livereload: true
+	});
 });
 
 gulp.task('watch', function () {
 	gulp.watch(['./styles/**/*.scss'], ['styles']);
-	gulp.watch(['./templates/**/*.tmpl'], ['templates']);
+	gulp.watch(['./templates/**/*.tmpl'], ['templates', 'scripts']);
 	gulp.watch(['./scripts/**/*.js'], ['scripts']);
 	gulp.watch(['./demo/src/**/*.jade', './demo/src/js/demo.js'], ['demo-views']);
 	gulp.watch(['./demo/src/styles/**/*.scss'], ['demo-styles']);
 });
 
-gulp.task('default', ['collect-components', 'styles', 'templates', 'scripts', 'demo-views', 'demo-styles', 'watch']);
+gulp.task('default', ['connect', 'collect-components', 'styles', 'templates', 'scripts', 'demo-views', 'demo-styles', 'watch']);
